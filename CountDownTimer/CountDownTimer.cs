78d532defc753a2ff04b14f139eac960076bd9c0
Ironlung 13 <56411088+Ironlung13@users.noreply.github.com>
2021-05-18 00:52:38 +0300

    Fix formatting

--- CountDownTimer/CountDownTimer.cs
+++ CountDownTimer/CountDownTimer.cs
@@ -11,7 +11,9 @@ namespace LabWork_Delegates_Timer
         {
             Name = name;
         }
+        
         private int _time;
+        
         public bool Running { get; private set; }
         public string Name { get; set; }
 
@@ -19,6 +21,7 @@ namespace LabWork_Delegates_Timer
         public event EventHandler<TimeLeftEventArgs> TimeLeftEvent;
         public event EventHandler<TimerEndEventArgs> TimerEndEvent;
         public event Action<CountDownTimer> UnsubscribeEvent;
+        
         public void SetupTimer(object sender, int time)
         {
             if (!Running)
@@ -27,20 +30,31 @@ namespace LabWork_Delegates_Timer
                 {
                     throw new ArgumentException("Can't setup timer using negative time value.");
                 }
-                else
+
+                try
                 {
-                    try
-                    {
-                        _time = time * 1000;
-                    }
-                    catch (OverflowException)
-                    {
-                        throw new OverflowException("Time value overflow.");
-                    }
+                    _time = checked(time * 1000);
                 }
+                catch (OverflowException)
+                {
+                    throw new OverflowException("Time value overflow.");
+                }
+                
                 StartTimer();
             }
         }
+        
+        public void UnsubscribeAll()
+        {
+            TimerStartEvent = null;
+            TimerEndEvent = null;
+            TimeLeftEvent = null;
+            if (!(UnsubscribeEvent is null))
+            {
+                UnsubscribeEvent(this);
+            }
+        }
+        
         private void StartTimer()
         {
             TimerStartEvent(this, new TimerStartEventArgs(_time, Name));
@@ -54,17 +68,6 @@ namespace LabWork_Delegates_Timer
                 _time -= 1000;
             }
             TimerEndEvent(this, new TimerEndEventArgs(Name));
-        }
-
-        public void UnsubscribeAll()
-        {
-            TimerStartEvent = null;
-            TimerEndEvent = null;
-            TimeLeftEvent = null;
-            if (!(UnsubscribeEvent is null))
-            {
-                UnsubscribeEvent(this);
-            }
         }
     }
 }
